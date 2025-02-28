eb4e53723f3f4f75bd8bb5a9f2daff9c80da3ef2
Qczer <119138392+Qczer@users.noreply.github.com>
2025-02-28 04:46:36 +0100

    Added basic van routing

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Vans() {
   const [vans, setVans] = React.useState([])
@@ -10,18 +11,23 @@ export default function Vans() {
 
   const vanElements = vans.map(van => (
     <div key={van.id} className="van-tile">
-        <img src={van.imageUrl} />
+      <Link to={`/vans/${van.id}`} aria-label={`View details for ${van.name}, priced at $${van.price} per day`}>
+        <img src={van.imageUrl} alt={van.name}/>
         <div className="van-info">
-            <h3>{van.name}</h3>
+            <p>{van.name}</p>
             <p>${van.price}<span>/day</span></p>
         </div>
         <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
     </div>
 ))
 
   return (
-    <>
-      {vanElements}
-    </>
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">
+        {vanElements}
+      </div>
+    </div>
   )
 }
